c0f0c0443c16b0ec57703b33c7cbd0438ff59d47
bc1q000 <plusplusfig@gmail.com>
2024-07-04 22:18:43 +0300

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-#1st program
+# 1st program
 print(9 ** 0.5 * 5)
 
-#2st program
+# 2st program
 print(9.99 > 9.98 and 1000 != 1000.1)
 
-#3st program
+# 3st program
 a = 1234
 b = 5678
 print((a // 10) % 100 + (b // 10) % 100)
 
-#4st program
+# 4st program
 a = 13.42
 b = 42.13
 print(a // 1 == b * 100 % 100 or b // 1 == a * 100 % 100)
