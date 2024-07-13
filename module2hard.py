910f54d27167eb3a4257d88c1d0ab2ee7ae40042
bc1q000 <plusplusfig@gmail.com>
2024-07-13 16:02:12 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,19 @@
+import random
+
+
+def password():
+    for i in range(1, 21):                    # 1й элемент i - наименьший
+        for j in range(i + 1, 21):            # 2й элемент не равный i и больше него на 1
+            if stone_slot[0] % (i + j) == 0:  # проверка делителя
+                a = f'{i}{j}'
+                stone_slot[1].append(a)       # добавление пары в список
+
+
+stone_slot = ['*', []]                        # Две каменные вставки для чисел. 2я - обязательно "список"
+stone_slot[0] = random.randint(3, 20)   # Изменение 1-го слота
+
+password()
+
+print(stone_slot[0], ':', sep='', end=' ')
+for item in stone_slot[1]:
+    print(item, end='')
