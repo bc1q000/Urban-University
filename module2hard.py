a1b33bd66c0122e5762afc04478d6e1dc2a64ae8
bc1q000 <plusplusfig@gmail.com>
2024-07-13 16:09:17 +0300

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -7,13 +7,11 @@ def password():
             if stone_slot[0] % (i + j) == 0:  # проверка делителя
                 a = f'{i}{j}'
                 stone_slot[1].append(a)       # добавление пары в список
-
+    print(stone_slot[0], ':', sep='', end=' ')
+    for item in stone_slot[1]:
+        print(item, end='')
 
 stone_slot = ['*', []]                        # Две каменные вставки для чисел. 2я - обязательно "список"
 stone_slot[0] = random.randint(3, 20)   # Изменение 1-го слота
 
 password()
-
-print(stone_slot[0], ':', sep='', end=' ')
-for item in stone_slot[1]:
-    print(item, end='')
