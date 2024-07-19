33ba2959b5a148634b7e252cbcc197eca298f34a
bc1q000 <plusplusfig@gmail.com>
2024-07-19 21:59:40 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -39,3 +39,5 @@ send_email('Это сообщение для проверки связи', 'vasy
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+'''Задача решена в соответствии с условиями задачи и приобретенными навыками'''
