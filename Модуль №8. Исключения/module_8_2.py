7c52a72a9914fd7e90e41c952b23a0bd035e523d
bc1q000 <plusplusfig@gmail.com>
2024-09-04 15:21:44 +0300

    Add files via upload

---
+++ Модуль №8. Исключения/module_8_2.py
@@ -0,0 +1,34 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+
+    for num in numbers:
+        try:
+            result += num
+        except TypeError:
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчета суммы - {num}')
+
+    return result, incorrect_data
+
+
+def calculate_average(numbers):
+    try:
+        result, incorrect_data = personal_sum(numbers)
+
+        count = len(numbers) - incorrect_data
+        if count == 0:
+            return 0
+        return result / count
+
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print('В numbers записан неверный тип данных')
+        return None
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Еще строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
