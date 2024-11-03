a2c7b2800585ff8878ba29e105e36214fce4ce4d
bc1q000 <plusplusfig@gmail.com>
2024-11-03 18:12:41 +0300

    Update module_13_6.py

--- Модуль №13. Основы асинхронного программирования на базе фреймворка aiogram/module_13_6.py
+++ Модуль №13. Основы асинхронного программирования на базе фреймворка aiogram/module_13_6.py
@@ -6,7 +6,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = '7298907113:AAH8ln3REKeXNpkPSke8TAOo2FJbFBmtJZA'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
